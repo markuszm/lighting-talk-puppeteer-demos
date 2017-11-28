2460e82f5f459d1116da8064213acc7143bdb924
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2017-11-28 16:44:03 +0100

    added puppeteer and headless config for karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,7 +29,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers: {
+      HeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--remote-debugging-port=9222']
+      }
+    },
     singleRun: false
   });
 };
