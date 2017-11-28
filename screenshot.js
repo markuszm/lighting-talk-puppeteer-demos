cd4cc14814f77b7e3f46015435da63c364f33492
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2017-11-28 18:46:36 +0100

    added demos for puppeteer

---
+++ screenshot.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+const browser = await puppeteer.launch({
+    args: ['--remote-debugging-port=9222']
+});
+const page = await browser.newPage();
+await page.emulate({viewport: {width: 1366, height: 768}, userAgent: "Chrome"})
+await page.goto('https://cosee.biz', {waitUntil: 'load'});
+await page.screenshot({path: 'cosee.png', fullPage: true});
+
+await browser.close();
+})();
